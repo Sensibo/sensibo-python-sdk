1732ff869dce263a65bdb6eb3e99990a79c058ee
Eric Gosselin <brutalboy69@gmail.com>
2015-09-14 14:47:38 -0400

    added a function called pod_lastX_ac_state
    pod_lastX_ac_state is returned last X states of the AC
    
    Also formatting to the LOCAL Time Zone

--- sensibo_client.py
+++ sensibo_client.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from datetime import datetime
+from dateutil import tz
 
 _SERVER = 'https://home.sensibo.com/api/v2'
 
@@ -31,7 +33,11 @@ class SensiboClientAPI(object):
     def pod_ac_state(self, podUid):
         result = self._get("/pods/%s/acStates" % podUid, limit = 1, fields="status,reason,acState")
         return result['result'][0]
-
+    
+    def pod_lastX_ac_state(self, podUid,nb):
+        result = self._get("/pods/%s/acStates" % podUid, limit = nb, fields="status,reason,time,acState")
+        return result['result']	
+        
     def pod_change_ac_state(self, podUid, on, target_temperature, mode, fan_level):
         self._post("/pods/%s/acStates" % podUid,
                 json.dumps({'acState': {"on": on, "targetTemperature": target_temperature, "mode": mode, "fanLevel": fan_level}}))
@@ -50,4 +56,20 @@ if __name__ == "__main__":
     print "Last AC change %(success)s and was caused by %(cause)s" % { 'success': 'was successful' if last_ac_state['status'] == 'Success' else 'failed', 'cause': last_ac_state['reason'] } 
     print "and set the ac to %s" % str(last_ac_state['acState'])
     print "Change AC state of %s" % pod_uids[0]
+    
+    #get the x last states
+    # Convert the date to local timezone
+    fmt = '%d/%m/%Y %H:%M:%S'
+    from_zone = tz.tzutc()
+    to_zone = tz.tzlocal()
+    
+    # Get the last 10 states of the AC
+    last5_ac_state = client.pod_lastX_ac_state(pod_uids[0],10)
+    for ac_state in last5_ac_state:
+        sstring = datetime.strptime(ac_state['time']['time'],'%Y-%m-%dT%H:%M:%SZ')
+        utc = sstring.replace(tzinfo=from_zone)
+        localzone = utc.astimezone(to_zone)
+        sdate = localzone.strftime(fmt)
+        print "Command executed at %(date)s : %(states" % { 'date' : sdate, 'state': str(ac_state['acState'])}
+    
     client.pod_change_ac_state(pod_uids[0], True, 23, 'cool', 'auto')
