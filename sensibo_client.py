ba9a9fd56fdcc31c5c6cec1542ca41f0ddb64656
John Paul Lorenti <jplorenti@gmail.com>
2018-01-27 19:21:13 -0500

    showState and showMeasurement command line options, catch Request HTTP errors

--- sensibo_client.py
+++ sensibo_client.py
@@ -38,19 +38,39 @@ class SensiboClientAPI(object):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Sensibo client example parser')
-    parser.add_argument('apikey', type = str)
-    parser.add_argument('--deviceName', type = str)
+    parser.add_argument('apikey', type = str, help='Obtain from https://home.sensibo.com/me/api')
+    parser.add_argument('--deviceName', type = str, help='Device name')
+    parser.add_argument('--showState', action='store_true',help='Display the AC state')
+    parser.add_argument('--showMeasurement', action='store_true',help='Display the sensor measurements')
     args = parser.parse_args()
+    #print(args)
 
     client = SensiboClientAPI(args.apikey)
-    devices = client.devices()
-    print ("-" * 10, "devices", "-" * 10)
-    print (devices)
-
+    try:
+      devices = client.devices()
+      print ("-" * 10, "devices", "-" * 10)
+      print (devices)
+    except requests.exceptions.RequestException as exc:
+      print ("Request failed with message %s" % exc)
+      exit(1)
+ 
     if(args.deviceName):
-      uid = devices[args.deviceName]
-      ac_state = client.pod_ac_state(uid)
-      print ("-" * 10, "AC State of %s" % args.deviceName, "_" * 10)
-      print (ac_state)
+      try: 
+        uid = devices[args.deviceName]
+        #Default to showing AC state since a device was specified without a clear reason
+        if(not args.showState and not args.showMeasurement):
+          args.showState=True 
+     
+        if(args.showState and uid):
+          ac_state = client.pod_ac_state(uid)
+          print ("-" * 10, "AC State of %s" % args.deviceName, "-" * 10)
+          print (ac_state)
+        if(args.showMeasurement and uid):
+          pod_measurement = client.pod_measurement(uid)
+          print ("-" * 10, "Measurement of %s" % args.deviceName, "-" * 10)
+          print (pod_measurement)
 
-#      client.pod_change_ac_state(uid, ac_state, "on", not ac_state['on']) 
+        #client.pod_change_ac_state(uid, ac_state, "on", not ac_state['on']) 
+      except requests.exceptions.RequestException as exc:
+        print ("Request failed with message %s" % exc)
+        exit(1)
